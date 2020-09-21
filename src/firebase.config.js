f56176fb529634954adb45c9cbeab409afe1d69b
Kazi Mamun <kazi299499@gmail.com>
2020-09-21 07:16:24 +0600

    first upload

---
+++ src/firebase.config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDcGd5OXLvMwNoKtDkphPZekdEoIXp2GGY",
+    authDomain: "second-ema-john.firebaseapp.com",
+    databaseURL: "https://second-ema-john.firebaseio.com",
+    projectId: "second-ema-john",
+    storageBucket: "second-ema-john.appspot.com",
+    messagingSenderId: "871387842646",
+    appId: "1:871387842646:web:c3332107872f2b82d413f7",
+    measurementId: "G-ZGXE4Q5RG5"
+};
+export default firebaseConfig;
